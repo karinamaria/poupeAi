42eed8bd9b8152845d23636ce0130971840a8572
karinamaria <karinamaria163@gmail.com>
2021-12-07 21:19:13 -0300

    first commit

---
+++ src/main/java/br/com/poupeAi/PoupeAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.poupeAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoupeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoupeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/poupeAi/PoupeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poupeAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoupeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
