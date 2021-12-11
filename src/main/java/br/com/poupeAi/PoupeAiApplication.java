da2a6e968b4935da680475b41ebd5889bbcbb951
karinamaria <karinamaria163@gmail.com>
2021-12-11 16:16:11 -0300

    feat: add authentication

--- src/main/java/br/com/poupeAi/PoupeAiApplication.java
+++ src/main/java/br/com/poupeAi/PoupeAiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PoupeAiApplication {
 
 	public static void main(String[] args) {
+//		BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+//		System.out.println(bc.encode("karina"));
 		SpringApplication.run(PoupeAiApplication.class, args);
 	}
 
