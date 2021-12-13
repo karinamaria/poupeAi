7f2f94d34055c426d26c4454d0357e639c14b441
mariaeloi <mariaeduardaeloi@outlook.com>
2021-12-13 18:32:00 -0300

    feat: add permission validation

--- src/main/java/br/com/poupeAi/repository/UsuarioRepository.java
+++ src/main/java/br/com/poupeAi/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package br.com.poupeAi.repository;
 
 import br.com.poupeAi.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends GenericRepository<Usuario> {
+    @Query(value = "SELECT u from Usuario u where u.email = ?1 and u.ativo=true")
     Usuario findByEmail(String email);
 }
--- src/main/java/br/com/poupeAi/service/UsuarioService.java
+++ src/main/java/br/com/poupeAi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package br.com.poupeAi.service;
 
 import br.com.poupeAi.exception.NegocioException;
+import br.com.poupeAi.exception.ResourceNotFoundException;
+import br.com.poupeAi.helper.UsuarioHelper;
 import br.com.poupeAi.model.Usuario;
 import br.com.poupeAi.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +13,36 @@ import java.util.Objects;
 
 @Service
 public class UsuarioService extends GenericService<Usuario, UsuarioRepository>  {
+    private final UsuarioHelper usuarioHelper;
+
     @Autowired
-    public UsuarioService(UsuarioRepository repository) {
+    public UsuarioService(UsuarioRepository repository, UsuarioHelper usuarioHelper) {
         this.nomeEntidade = "Usuário";
         this.repository = repository;
+        this.usuarioHelper = usuarioHelper;
     }
 
     @Override
     public void validar(Usuario usuario) throws NegocioException {
+        if(Objects.nonNull(usuario.getId()) && !usuarioHelper.getUsuarioLogado().getId().equals(usuario.getId()))
+            throw  new NegocioException("Você não tem permissão para editar esse usuário.");
+
         Usuario usuarioEncontrado = this.buscarPorEmail(usuario);
         if(Objects.nonNull(usuarioEncontrado) && !usuarioEncontrado.getId().equals(usuario.getId()))
             throw new NegocioException("Este endereço de e-mail já está em uso.");
     }
 
     @Override
+    public void validarNaRemocao(Usuario usuario) throws NegocioException, ResourceNotFoundException {
+        if(Objects.nonNull(usuario.getId()) && !usuarioHelper.getUsuarioLogado().getId().equals(usuario.getId()))
+            throw  new NegocioException("Você não tem permissão para remover esse usuário.");
+    }
+
+    @Override
     public Usuario salvar(Usuario usuario) throws NegocioException {
-        // Criptografar a senha do usuário
-        usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
+        if(Objects.isNull(usuario.getId()) || !this.buscarPorId(usuario.getId()).getSenha().equals(usuario.getSenha()))
+            // Criptografar a senha do usuário
+            usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
 
         return super.salvar(usuario);
     }
