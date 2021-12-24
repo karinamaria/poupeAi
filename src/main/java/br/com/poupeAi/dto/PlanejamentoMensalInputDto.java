de01fb716951293b0096082c16e7951d05400f3a
karinamaria <karinamaria163@gmail.com>
2021-12-24 09:23:00 -0300

    refac: remove field of dto's planejamento

--- src/main/java/br/com/poupeAi/dto/PlanejamentoMensalInputDto.java
+++ src/main/java/br/com/poupeAi/dto/PlanejamentoMensalInputDto.java
@@ -12,9 +12,9 @@ import javax.validation.constraints.Positive;
 @Getter @Setter
 @NoArgsConstructor @EqualsAndHashCode
 public class PlanejamentoMensalInputDto {
-    @NotNull
-    @Positive
-    private int frequenciaEnvioRelatorio;
+//    @NotNull
+//    @Positive
+//    private int frequenciaEnvioRelatorio;
 
     @NotNull
     @Min(value=1)
@@ -24,34 +24,4 @@ public class PlanejamentoMensalInputDto {
     @NotNull
     @Min(value=2021)
     private int ano;
-
-    public PlanejamentoMensalInputDto(int frequenciaEnvioRelatorio, int mes, int ano) {
-        this.frequenciaEnvioRelatorio = frequenciaEnvioRelatorio;
-        this.mes = mes;
-        this.ano = ano;
-    }
-
-    public int getFrequenciaEnvioRelatorio() {
-        return frequenciaEnvioRelatorio;
-    }
-
-    public void setFrequenciaEnvioRelatorio(int frequenciaEnvioRelatorio) {
-        this.frequenciaEnvioRelatorio = frequenciaEnvioRelatorio;
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
 }
